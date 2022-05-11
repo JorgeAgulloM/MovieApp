4e645aa4ae7996f868c2edf0f185728c86141474
Jorge Agullo <agullojorge@gmail.com>
2022-05-11 17:45:38 +0200

    [FEATURE][WIP] Added LazyRow to see images of film

--- app/src/main/java/com/practice/movieapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/practice/movieapp/screens/details/DetailsScreen.kt
@@ -1,7 +1,10 @@
 package com.practice.movieapp.screens.details
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -10,8 +13,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
 import com.practice.movieapp.model.Movie
 import com.practice.movieapp.model.getMovies
+import com.practice.movieapp.widgets.MovieRow
 
 @Composable
 fun DetailsScreen(
@@ -45,19 +50,31 @@ fun DetailsScreen(
         ) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                verticalArrangement = Arrangement.Top
             ) {
-
-                Text(
+                MovieRow(movie = newMovieList.first())
+                /*Text(
                     text = newMovieList[0].title,
                     style = MaterialTheme.typography.h5
-                )
-                /*Spacer(modifier = Modifier.height(23.dp))
-                Button(onClick = {
-                    navController.popBackStack()
-                }) {
-                    Text(text = "Go Back")
-                }*/
+                )*/
+                Spacer(modifier = Modifier.height(8.dp))
+                Divider()
+                Text(text = "Movie Images")
+                LazyRow {
+                    items(newMovieList[0].images) { image ->
+                        Card(
+                            modifier = Modifier
+                                .padding(12.dp)
+                                .size(240.dp),
+                            elevation = 5.dp
+                        ) {
+                            Image(
+                                painter = rememberAsyncImagePainter(model = image),
+                                contentDescription = "Movie Poster"
+                            )
+                        }
+                    }
+                }
             }
         }
     }
