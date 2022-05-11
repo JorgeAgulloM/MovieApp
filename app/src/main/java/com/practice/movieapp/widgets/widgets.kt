4d8ba209fccc5377da4c032dc057d42753d379e5
Jorge Agullo <agullojorge@gmail.com>
2022-05-11 17:25:22 +0200

    [FEATURE][WIP] Added extra info & expand button for row custom

--- app/src/main/java/com/practice/movieapp/widgets/widgets.kt
+++ app/src/main/java/com/practice/movieapp/widgets/widgets.kt
@@ -9,14 +9,20 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import coil.transform.CircleCropTransformation
@@ -37,7 +43,7 @@ fun MovieRow(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(130.dp)
+            //.height(if (!expanded) 130.dp else 230.dp)
             .clickable {
                 onItemClick(movie.id)
             },
@@ -89,13 +95,42 @@ fun MovieRow(
 
                 AnimatedVisibility(visible = expanded) {
                     Column() {
-                        Text(text = "Hello there")
+                        Text( buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray,
+                                    fontSize = 13.sp
+                                )
+                            ) {
+                                append("Plot: ")
+                            }
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray,
+                                    fontSize = 13.sp,
+                                    fontWeight = FontWeight.Light
+                                )
+                            ) {
+                                append(movie.plot)
+                            }
+                        }, modifier = Modifier.padding(6.dp))
+
+                        Divider(modifier = Modifier.padding(3.dp))
+                        Text(text = "Director: ${movie.director}", style = MaterialTheme.typography.caption)
+                        Text(text = "Actors: ${movie.actors}", style = MaterialTheme.typography.caption)
+                        Text(text = "Rating: ${movie.rating}", style = MaterialTheme.typography.caption)
+
                     }
                 }
 
                 Icon(
-                    imageVector = Icons.Filled.KeyboardArrowDown,
-                    contentDescription = "Down Arrow", modifier = Modifier
+                    imageVector =
+                    if (expanded)
+                        Icons.Filled.KeyboardArrowUp
+                    else
+                        Icons.Filled.KeyboardArrowDown,
+                    contentDescription = "Down Arrow",
+                    modifier = Modifier
                         .size(25.dp)
                         .clickable {
                             expanded = !expanded
