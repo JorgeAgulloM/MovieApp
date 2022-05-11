64509051753d65879f47b11d0738b73858708c3b
Jorge Agullo <agullojorge@gmail.com>
2022-05-11 13:01:45 +0200

    [FEATURE][WIP] Added Controller

---
+++ app/src/main/java/com/practice/movieapp/screens/homr/HomeScreen.kt
@@ -0,0 +1,7 @@
+package com.practice.movieapp.screens.homr
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(navController: NavController) {}
