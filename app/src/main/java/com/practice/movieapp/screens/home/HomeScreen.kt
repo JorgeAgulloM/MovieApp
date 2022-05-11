897a0f904363c108c946c975cc5f47bfd32e75e0
Jorge Agullo <agullojorge@gmail.com>
2022-05-11 13:44:42 +0200

    [FEATURE][WIP] Navigation Works

--- app/src/main/java/com/practice/movieapp/screens/home/HomeScreen.kt
+++ app/src/main/java/com/practice/movieapp/screens/home/HomeScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.practice.movieapp.MovieRow
+import com.practice.movieapp.navigation.MovieScreens
 
 @Composable
 fun HomeScreen(navController: NavController) {
@@ -45,7 +46,7 @@ fun MainContent(
             items(items = movieList) { movie -> //movie sustituye a it
                 MovieRow(movie = movie) { resultMovie ->
 
-                    Log.d("TAG", "MainContent: $resultMovie")
+                    navController.navigate(route = MovieScreens.DetailsScreen.name)
 
                 }
             }
