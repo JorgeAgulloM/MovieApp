1088bbdc0b5156ded19563ff8569f78df0846773
Jorge Agullo <agullojorge@gmail.com>
2022-05-11 17:02:38 +0200

    [FEATURE][WIP] Added colapse column extra info

--- app/src/main/java/com/practice/movieapp/widgets/widgets.kt
+++ app/src/main/java/com/practice/movieapp/widgets/widgets.kt
@@ -1,20 +1,23 @@
 package com.practice.movieapp.widgets
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
-import coil.compose.rememberImagePainter
 import coil.request.ImageRequest
 import coil.transform.CircleCropTransformation
 import com.practice.movieapp.model.Movie
@@ -27,6 +30,9 @@ fun MovieRow(
     movie: Movie = getMovies()[0],
     onItemClick: (String) -> Unit = {}
 ) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
     Card(
         modifier = Modifier
             .padding(4.dp)
@@ -81,7 +87,23 @@ fun MovieRow(
                     style = MaterialTheme.typography.caption
                 )
 
+                AnimatedVisibility(visible = expanded) {
+                    Column() {
+                        Text(text = "Hello there")
+                    }
+                }
+
+                Icon(
+                    imageVector = Icons.Filled.KeyboardArrowDown,
+                    contentDescription = "Down Arrow", modifier = Modifier
+                        .size(25.dp)
+                        .clickable {
+                            expanded = !expanded
+                        },
+                    tint = Color.DarkGray
+                )
             }
+
         }
 
 
